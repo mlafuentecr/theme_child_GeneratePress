d31e27d4eacf622ad1af9381d26a395a167175a8
mario Lafuente <45186223+mlafuentecr@users.noreply.github.com>
2026-02-18 23:02:24 -0600

    Refactor child theme to clean minimal GeneratePress starter

---
+++ inc/setup.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Basic theme setup.
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+add_action('after_setup_theme', function (): void {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus([
+        'primary' => __('Primary Menu', 'generatepress-child'),
+        'footer'  => __('Footer Menu', 'generatepress-child'),
+    ]);
+});
